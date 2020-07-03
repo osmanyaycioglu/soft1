48c140f7e0de60ee6f997385abedcdc1ec53bd82
Osman <osman.yaycioglu@gmail.com>
2020-07-03 16:33:11 +0300

    commit 4

---
+++ ms-order/src/main/java/com/soft/order/MsOrderApplication.java
@@ -0,0 +1,15 @@
+package com.soft.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MsOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOrderApplication.class, args);
+	}
+
+}
---
+++ ms-order/src/main/java/com/soft/order/Order.java
@@ -0,0 +1,35 @@
+package com.soft.order;
+
+import javax.validation.constraints.Size;
+import java.util.List;
+
+public class Order {
+    private long customerId;
+    private int amount;
+    @Size(min = 1,message = "En az bir tane yemek ismi olmalı")
+    private List<String> orderItems;
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(final long customerIdParam) {
+        customerId = customerIdParam;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(final int amountParam) {
+        amount = amountParam;
+    }
+
+    public List<String> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(final List<String> orderItemsParam) {
+        orderItems = orderItemsParam;
+    }
+}
