f9f58755523aeb50555f96becca6bc816202b9ac
Osman <osman.yaycioglu@gmail.com>
2020-07-03 15:42:49 +0300

    Day 4 2

---
+++ node-discovery-server/src/main/java/com/soft/nodediscoveryserver/NodeDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.soft.nodediscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NodeDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodeDiscoveryServerApplication.class, args);
+	}
+
+}
